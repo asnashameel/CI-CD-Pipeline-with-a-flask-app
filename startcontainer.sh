1d477ad261c3ed41cbc2fa6d0fa1c468de49dea5
mr2chowd <mr2chowd@uwaterloo.ca>
2024-07-01 14:10:07 -0400

    newbyu

--- startcontainer.sh
+++ startcontainer.sh
@@ -4,7 +4,7 @@ set -e
 sudo apt install docker.io -y
 
 ### pulling the docker image
-docker pull mr2chowdh/flaskapp:latest
+docker pull mr2chowd/flaskapp:latest
 
 ## running the docker application at port 5000
 docker run -d -p 5000:5000
